1b6714e49d6cb0205f388f8afa621cdb10fe1b51
Vinuri Rodrigo <77739211+VINURIRODRIGO@users.noreply.github.com>
2022-05-02 21:36:01 +0530

    Update amplify-dependent-resources-ref.d.ts

--- SignIn and SignOut/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ SignIn and SignOut/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
     "auth": {
-        "Parkit": {
+        "Techpulse": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
             "UserPoolId": "string",
